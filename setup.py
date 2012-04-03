64cff45311e87822d366e5d92a638b9cafef5dbb
Alexei Andrushievich <madreaper@gmail.com>
2012-04-03 15:08:47 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,10 @@ import os
 import warnings
 from setuptools import setup, find_packages
 
-from notification_jabber import __version__
+from notification_jabber import (
+    __version__,
+    __license__,
+)
 
 setup(
     name = "notification_jabber",
@@ -17,13 +20,13 @@ setup(
     scripts = ['notification_jabber.py'],
     install_requires = ['docutils', 'xmpppy', ],
     package_data = {
-    	'': ['*.rst', '*.ini', 'COPYING', ],
+    	'notification_jabber': ['*.rst', '*.ini', 'COPYING', ],
     },
     author = "Alexei Andrushievich",
     author_email = "vint21h@vint21h.pp.ua",
     description = "Nagios-plugin that send Nagios notifications via jabber",
-     long_description = open('README.rst').read(),
-    license = "GPLv2 or later",
+    long_description = open('README.rst').read(),
+    license = __license__,
     url = "https://github.com/vint21h/notification_jabber",
     zip_safe = False,
     include_package_data = True,
